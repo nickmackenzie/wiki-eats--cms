73127b9153704942af54a811cffec443e8bb55c3
Nick Mackenzie <mackenzie.nick@outlook.com>
2022-01-03 00:08:56 -0500

    model creation

---
+++ src/api/landing-text/controllers/landing-text.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  landing-text controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::landing-text.landing-text');
---
+++ src/api/landing-text/routes/landing-text.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * landing-text router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::landing-text.landing-text');
