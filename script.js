8bd9156552e874b68760fed08aead3724a45a037
Nick Mackenzie <mackenzie.nick@outlook.com>
2022-01-03 00:51:15 -0500

    more schemas

---
+++ script.js
@@ -0,0 +1,6 @@
+//get all buisiness  from json
+// loop through buiensss
+//loop through category
+//if category doesnt exist, add to db, than add id to buiness
+//if excist, add id to buinesins
+//import buiness
